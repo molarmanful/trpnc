f1d1919c5d31a307394ec3630b9b86f0907afe72
Benjamin Pang <benjinpang@gmail.com>
2017-03-07 07:14:02 -0800

    first commits

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # trpnc
-Stack-based calculator for the terminal.
+**trpnc** (Terminal Reverse Polish Notation Calculator, pronounced "tar pink") is a stack-based calculator for the terminal. It is more of a personal calculator, but is pretty customizable.
---
+++ index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+K=require('keypress')
+O=require('ansi')(process.stdout)
+E=require('child_process').execSync
+stack=['']
+
+process.stdout.write('\033c')
+K(process.stdin)
+
+sto=_=>(
+  process.stdout.write('\033c'+stack.slice(0).reverse().join`\n`),
+  O.goto(stack[0].length+1,stack.length)
+)
+
+process.stdin.on('keypress',(a,b)=>{
+  a=='q'||a=='\u001b'?
+    process.stdin.pause()
+  :a.match(/^[\d.e]+$/)?
+    (stack[0]+=a)
+  :(a==' '||a=='\r')&&stack[0]?
+    (stack[0]=eval(stack[0])+'',stack.unshift(''))
+  :a=='_'?
+    (stack[0]=''+-stack[0])
+  :b&&b.name=='backspace'?
+    (stack[0]=stack[0].slice(0,-1))
+  :a.match(/^[+\-*/%^]$/)&&stack.length>1?
+    (stack[0]||stack.shift(),stack.unshift('',eval(`${stack.splice(1,1)} ${a.replace('^','**')} ${stack.shift()}`)))
+  :a=='v'?
+    (stack[0]**=.5)
+  :0
+  sto()
+})
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
