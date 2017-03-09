3e770ff58994fe31a406cec93d640034c614cad2
Benjamin Pang <benjinpang@gmail.com>
2017-03-09 13:41:53 -0800

    add undo/redo

--- index.js
+++ index.js
@@ -2,40 +2,81 @@
 K=require('keypress')
 O=require('ansi')(process.stdout)
 E=require('child_process').execSync
+I=require('prompt-sync')()
+R=require('robotjs')
 stack=['']
+und=[stack]
+red=[]
 
-process.stdout.write('\033c')
+O.write('\033c0\t')
 K(process.stdin)
 
-sto=_=>(
-  stack=stack.filter((x,y)=>x||!y),
-  process.stdout.write('\033c'+stack.slice(0).reverse().join`\n`),
-  O.goto(stack[0].length+1,stack.length)
-)
+sto=_=>{
+  _!='u'&&_!='r'?(und.push(stack),red=[]):(stack=und[und.length-1])
+  stack=stack&&stack.filter((a,b)=>a||!b)
+  O.write('\033c'+stack.map((a,b)=>b+'\t'+a).reverse().join`\n`)
+}
 
-process.stdin.on('keypress',(a,b)=>{
-  a=='q'||a=='\u001b'?
-    process.stdin.pause()
-  :a.match(/^[\d.]+$/)||a=='e'&&stack[0]?
+//ex mode
+exec=x=>{
+  O.reset()
+  x.split` +`.map(a=>{
+    a=='q'?
+      process.exit(0)
+
+    //bulk stack commands
+    :a=='r'?
+      (stack.reverse(),stack.unshift(''))
+    :a=='c'?
+      (stack=[''])
+
+    :0
+  })
+}
+
+//main process
+process.stdin.on('keypress',key=(a='',b='')=>{
+  //num
+  a.match(/^[\d.]+$/)||a=='e'&&stack[0]?
     (stack[0]+=a)
   :(a==' '||a=='\r')&&stack[0]?
     (stack[0]=eval(stack[0])+'',stack.unshift(''))
   :a=='_'?
     (stack[0]=''+-stack[0])
-  :b&&b.name=='backspace'?
-    (stack[0]=stack[0].slice(0,-1))
+  :a=='backspace'||(b.name=='backspace'&&(stack[0]||stack[1]))?
+    stack[0]?(stack[0]=stack[0].slice(0,-1)):stack.shift()
+
+  //undo/redo
+  :a=='u'&&und.length>1?
+    red.push(und.pop())
+  :a=='r'&&red.length?
+    und.push(red.pop())
+
+  //math
   :a.match(/^[+\-*/%^]$/)&&stack.length>1?
     (stack[0]||stack.shift(),stack.unshift('',eval(`${stack.splice(1,1)} ${a.replace('^','**')} ${stack.shift()}`)))
   :a=='v'?
-    (stack[0]**=.5)
+    (stack[0]**=.5,stack.unshift(''))
+
+  //stack
   :a=='$'&&(stack[0]||stack[1])?
     stack.unshift(stack[0]||stack[1])
   :a=='!'&&(stack[0]||stack[1])?
     (stack[0]||stack.shift(),stack[0]='')
   :a=='\\'&&stack[1]?
-    (stack[0]||stack.shift(),stack.unshift(stack.splice(1,1)))
+    (stack[0]||stack.shift(),stack.unshift('',stack.splice(1,1)))
+  :a=='@'&&stack[2]?
+    (stack[0]||stack.shift(),stack.splice(stack.length>2?2:1,0,stack.shift()),stack.unshift(''))
+  :a=='#'&&stack[1]?
+    stack.unshift('',stack[1])
+
+  //ex mode
+  :a==';'?
+    (stack[0]?O.write('\n'):stack.shift(),process.stdin.pause(),O.green().write('\r\t\r'),exec(I(';')||''),process.stdin.resume(),stack.unshift(''))
+
   :0
-  sto()
+
+  sto(a)
 })
 
 process.stdin.setRawMode(true);
