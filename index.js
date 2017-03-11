1803fde5b19851ed41a438ffe68188ef93c8c97c
Benjamin Pang <benjinpang@gmail.com>
2017-03-11 09:12:40 -0800

    use latest prompt-sync lib for copy-paste

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ E=require('child_process').execSync
 I=require('prompt-sync')()
 R=require('robotjs')
 stack=['']
+vars={}
 und=[stack]
 red=[]
 
@@ -14,7 +15,7 @@ K(process.stdin)
 sto=_=>{
   _!='u'&&_!='r'?(und.push(stack),red=[]):(stack=und[und.length-1])
   stack=stack&&stack.filter((a,b)=>a||!b)
-  O.write('\033c'+stack.map((a,b)=>b+'\t'+a).reverse().join`\n`)
+  O.reset().write('\033c'+stack.map((a,b)=>b+'\t'+a).reverse().join`\n`)
 }
 
 //ex mode
@@ -53,14 +54,16 @@ process.stdin.on('keypress',key=(a='',b='')=>{
     und.push(red.pop())
 
   //math
-  :a.match(/^[+\-*/%^]$/)&&stack.length>1?
-    (stack[0]||stack.shift(),stack.unshift('',eval(`${stack.splice(1,1)} ${a.replace('^','**')} ${stack.shift()}`)))
+  :a.match(/^[+\-*/%]$/)&&stack.length>1?
+    (stack[0]||stack.shift(),stack.unshift('',eval(`${stack.splice(1,1)} ${a} ${stack.shift()}`)))
+  :a=='^'&&stack.length>1?
+    (stack[0]||stack.shift(),stack.unshift('',Math.pow(stack.splice(1,1),stack.shift())))
   :a=='v'?
-    (stack[0]**=.5,stack.unshift(''))
+    (stack[0]=Math.sqrt(stack[0]),stack.unshift(''))
 
   //stack
   :a=='$'&&(stack[0]||stack[1])?
-    stack.unshift(stack[0]||stack[1])
+    (stack[0]||stack.shift(),stack.unshift('',stack[0]))
   :a=='!'&&(stack[0]||stack[1])?
     (stack[0]||stack.shift(),stack[0]='')
   :a=='\\'&&stack[1]?
@@ -72,7 +75,25 @@ process.stdin.on('keypress',key=(a='',b='')=>{
 
   //ex mode
   :a==';'?
-    (stack[0]?O.write('\n'):stack.shift(),process.stdin.pause(),O.green().write('\r\t\r'),exec(I(';')||''),process.stdin.resume(),stack.unshift(''))
+    (
+      stack[0]?O.write('\n'):stack.shift(),process.stdin.pause(),O.green().write('\r\t\r'),
+      v=I(';')||'',exec(v),
+      process.stdin.resume(),stack.unshift('')
+    )
+
+  //vars
+  :a=='='?
+    (
+      stack[0]?O.write('\n'):stack.shift(),process.stdin.pause(),O.blue().write('\r\t\r'),
+      v=I('='),v&&stack[0]&&(vars[v]=stack.shift()),
+      process.stdin.resume(),stack.unshift('')
+    )
+  :a=='|'?
+    (
+      stack[0]?O.write('\n'):stack.shift(),process.stdin.pause(),O.blue().write('\r\t\r'),
+      v=I('|'),vars[v]&&stack.unshift(vars[v]),
+      process.stdin.resume(),stack.unshift('')
+    )
 
   :0
 
